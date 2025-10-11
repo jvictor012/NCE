23407452b48eaec4c53a17b2c783a041f1a06e45
João Victor <joaovictorqueimadas@gmail.com>
2025-10-11 13:32:37 -0300

    adicionando pagina do usuario e perfil

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+
+        return render_template('index.htlm')
     return render_template('index.html')
+
+
+@app.route('/perfil', methods=['GET', 'POST'])
+def perfil():
+    if request.method == "POST":
+        return render_template('perfil.html')
+    else:
+        return render_template('perfil.html')
 
 
 
@@ -22,7 +33,7 @@ def login():
     if request.method == 'POST':
         matricula = request.form['lmatricula']
         senha = request.form['lsenha']
-        return render_template('login.html', mat = matricula, sen = senha)
+        return render_template('index.html', mat = matricula, sen = senha)
     return render_template('login.html')
 
 
